e04a4cd324cbcc63ad64fb83914eec7b202f28d9
Boyko Amarov <boyko.amarov@gmail.com>
2018-05-22 10:57:47 +0300

    Initial state needLimitMeasure set to false.

--- src/localizers/default.js
+++ src/localizers/default.js
@@ -1,8 +1,9 @@
 const localizer = {};
 
-const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+// TODO: this is not portable...
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-localizer.startOfWeek = () => 0;
+localizer.startOfWeek = () => 1;
 localizer.format = (day, format) => {
   if (format === 'ddd') {
     return weekDays[day.getDay()];
